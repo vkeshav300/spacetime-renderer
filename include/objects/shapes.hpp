cf3b14b3c838fc7ff56da96cd342e6a7949a83be
Keshav Verma <vkeshav300@gmail.com>
2025-03-25 20:56:49 -0700

    Format: code

--- include/objects/shapes.hpp
+++ include/objects/shapes.hpp
@@ -7,17 +7,16 @@
 using vertex_vector = std::vector<Vertex>;
 
 namespace shapes {
-    const vertex_vector triangle = {
-        {{{-0.5f, -0.5f, 0.0f, 1.0f}, {0.0f, 0.0f}},
-         {{0.5f, -0.5f, 0.0f, 1.0f}, {0.0f, 0.0f}},
-         {{0.0f, 0.5f, 0.0f, 1.0f}, {0.0f, 0.0f}}}};
+const vertex_vector triangle = {{{{-0.5f, -0.5f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+                                 {{0.5f, -0.5f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+                                 {{0.0f, 0.5f, 0.0f, 1.0f}, {0.0f, 0.0f}}}};
 
-    const vertex_vector square = {{
-        {{-0.5f, -0.5f, 0.5f, 1.0f}, {0.0f, 0.0f}}, // Bottom-left
-        {{-0.5f, 0.5f, 0.5f, 1.0f}, {0.0f, 1.0f}},  // Top-left
-        {{0.5f, 0.5f, 0.5f, 1.0f}, {1.0f, 1.0f}},   // Top-right
-        {{-0.5f, -0.5f, 0.5f, 1.0f}, {0.0f, 0.0f}}, // Bottom-left (again)
-        {{0.5f, 0.5f, 0.5f, 1.0f}, {1.0f, 1.0f}},   // Top-right
-        {{0.5f, -0.5f, 0.5f, 1.0f}, {1.0f, 0.0f}}   // Bottom-right
-    }};
-};
+const vertex_vector square = {{
+    {{-0.5f, -0.5f, 0.5f, 1.0f}, {0.0f, 0.0f}}, // Bottom-left
+    {{-0.5f, 0.5f, 0.5f, 1.0f}, {0.0f, 1.0f}},  // Top-left
+    {{0.5f, 0.5f, 0.5f, 1.0f}, {1.0f, 1.0f}},   // Top-right
+    {{-0.5f, -0.5f, 0.5f, 1.0f}, {0.0f, 0.0f}}, // Bottom-left (again)
+    {{0.5f, 0.5f, 0.5f, 1.0f}, {1.0f, 1.0f}},   // Top-right
+    {{0.5f, -0.5f, 0.5f, 1.0f}, {1.0f, 0.0f}}   // Bottom-right
+}};
+}; // namespace shapes
